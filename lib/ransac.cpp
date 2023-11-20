bf18667d8af5e5e6e0c3991748ca7d7c8b130225
msangalli <mateussangalli@gmail.com>
2023-11-20 18:46:28 +0100

    Add more comments on the fit function

--- lib/ransac.cpp
+++ lib/ransac.cpp
@@ -4,7 +4,7 @@
 #include <random>
 
 // Samples num_samples rows from the data.
-Eigen::MatrixXf SampleRows(const Eigen::MatrixXf & data, int num_samples) {
+Eigen::MatrixXf sampleRows(const Eigen::MatrixXf & data, int num_samples) {
     std::vector<int> row_indices(data.rows());
     std::iota(row_indices.begin(), row_indices.end(), 0); // Fill with 0, 1, 2, ..., numRows-1
     std::shuffle(row_indices.begin(), row_indices.end(), std::mt19937(std::random_device()()));
@@ -47,17 +47,20 @@ EstimatorRANSAC::EstimatorRANSAC(std::unique_ptr<ModelInterface> & model,
 void EstimatorRANSAC::fit() {
     for (int i=0; i < num_iter; i++) {
 
-        auto maybe_inliers = SampleRows(*data, num_samples);
+        // sample num_samples from the data and fit the model
+        auto maybe_inliers = sampleRows(*data, num_samples);
         model->fit(maybe_inliers);
 
         auto errors = model->errors(*data);
 
-
+        // from the errors obtained by the model at every sample, determine
+        // the inliers and outliers according to the tolerance
         int current_index = 0;
         std::vector<int> inlier_indices, outlier_indices;
         for (auto error_value: errors.rowwise()) {
             float value = error_value.data()[0];
 
+            // save the indices of the inliers and outliers
             if(value < tolerance) {
                 inlier_indices.push_back(current_index);
             } else {
@@ -66,15 +69,19 @@ void EstimatorRANSAC::fit() {
             current_index += 1;
         }
 
-
+        // if we have at least min_inliers inliers, the current model is considered a 'good' model
+        // and we can go to the next step, otherwise try again
         if(inlier_indices.size() < min_inliers) { continue; }
 
+        // select the inliers from the inlier indices
+        // and fit the model with them
         auto inliers = gatherRows(*data, inlier_indices);
+        model->fit(inliers); // we assume that fit also does the work of resetting the model in this step
 
-        model->fit(inliers);
-
+        // compute the mean error of this model on the inliers
         float mean_error = model->errors(inliers).sum();
 
+        // if the mean error is better than the current best error, save the model, error and inliers/outliers
         if(mean_error < best_error) {
             best_error = mean_error;
             best_model.swap(model);
